2d9246429ddced3c93381541bebe5e98462676c1
LabibIslam <labib.islam.practice@gmail.com>
2022-02-15 18:33:18 +0600

    added some banking commands in js file and completed balance side

--- JS/bank.js
+++ JS/bank.js
@@ -1,4 +1,26 @@
 //handle deposit button event
 document.getElementById('deposit-button').addEventListener('click',function(){
-    console.log('deposit button clicked');
+    //get the amount deposited
+    const depositInput = document.getElementById('deposit-input');
+    const newDepositeAmountText = depositInput.value;
+    const newDepositeAmount = parseFloat(newDepositeAmountText);
+
+    const depositTotal = document.getElementById('deposit-total');
+    const previousDepositText = depositTotal.innerText;
+    const previousDepositAmount = parseFloat(previousDepositText);
+    const newDepositTotal = previousDepositAmount + newDepositeAmount;
+
+    depositTotal.innerText = newDepositTotal;
+
+    //update account balance
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+
+    const newBalanceTotal = previousBalanceTotal + newDepositeAmount;
+
+    balanceTotal.innerText = newBalanceTotal;
+
+    //clear deposit input field
+    depositInput.value = '';
 })
