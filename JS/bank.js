d72b87d1415f22d42fc660e64ce90aa6e3884334
LabibIslam <labib.islam.practice@gmail.com>
2022-02-15 17:04:33 +0600

    added banking page and also added bank js file

--- JS/bank.js
+++ JS/bank.js
@@ -1,15 +1,4 @@
-document.getElementById('login-submit').addEventListener('click', function(){
-    
-    //get user email
-    const emailField = document.getElementById('user-email');
-    const userEmail = emailField.value;
-    
-    //get user password
-    const passwordField =document.getElementById('user-password');
-    const userPassord = passwordField.value;
-    
-    //check email and password
-    if(userEmail == 'sontan@baap.com' && userPassord == 'secret'){
-        window.location.href='banking.html'
-    }
-});
+//handle deposit button event
+document.getElementById('deposit-button').addEventListener('click',function(){
+    console.log('deposit button clicked');
+})
